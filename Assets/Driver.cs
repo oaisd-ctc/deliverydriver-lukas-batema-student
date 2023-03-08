aef63c9c709cf5fddb197f072303775cc61a2e3e
Lukas Batema <lubaf18@student.oaisd.org>
2023-03-08 14:35:56 -0500

    Test

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-    [SerializeField] float steerSpeed = 1f;
-    [SerializeField] float moveSpeed = 0.01f;
+    [SerializeField] float steerSpeed = 150f;
+    [SerializeField] float moveSpeed = 25f;
 
     // Start is called before the first frame update
     void Start()
